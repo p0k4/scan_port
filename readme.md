cebbc14871265f8b719000cb765120d510d30789
Antonio Martins <tewmartins@gmail.com>
2022-06-09 15:22:34 +0100

    add

---
+++ readme.md
@@ -0,0 +1,1 @@
+Criação de script para procurar "scan" de portas abertas numa rede!
---
+++ scan.sh
@@ -0,0 +1,12 @@
+
+#!/bin/bash
+# Script para procurar portas abertas na rede.
+clear
+echo -n "rede?"
+read rede
+echo -n "porta?"
+read porta
+
+echo A executar scan de hosts na rede $rede com a porta $porta ...
+
+nmap -sS -vv -p $porta $rede | grep "Discovered open port" | awk '{print 6}'
